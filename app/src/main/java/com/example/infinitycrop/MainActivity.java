1639c0faee0de68d141b2d362b6e1c214d4514d1
Andrey <andreykuzmin342@gmail.com>
2020-10-28 18:53:24 +0100

    Dasboard + menu -> Terminado (arreglado menu bug arriba)

--- app/src/main/java/com/example/infinitycrop/MainActivity.java
+++ app/src/main/java/com/example/infinitycrop/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.AppThemeHome);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chipNavigationBar=findViewById(R.id.chipNavigation);
