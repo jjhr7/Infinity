055439275ed7adf3c2222f7dc07394a44c98f156
Andrey <andreykuzmin342@gmail.com>
2020-10-30 11:48:50 +0100

    App v1.3 full mango

--- app/src/main/java/com/example/infinitycrop/ui/login/LogActivity.java
+++ app/src/main/java/com/example/infinitycrop/ui/login/LogActivity.java
@@ -72,7 +72,7 @@ public class LogActivity extends AppCompatActivity implements GoogleApiClient.On
     }
 
     private void goMainScreen() {
-       Intent intent=new Intent(this, RegisterActivity.class);
+       Intent intent=new Intent(this, MainActivity.class);
        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
        startActivity(intent);
     }
