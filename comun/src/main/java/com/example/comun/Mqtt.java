7bbd1902a861cf1c4fbcb64da78fd41c2e2a163f
Quiqueferre12 <quiqueferre12@gmail.com>
2020-11-17 10:21:55 +0100

    Implementado  comunicación vía Mqtt en android things con firestore

---
+++ comun/src/main/java/com/example/comun/Mqtt.java
@@ -0,0 +1,11 @@
+package com.example.comun;
+
+public class Mqtt {
+    public static final String TAG = "MQTT";
+    public static final String topicRoot="infinity/prueba/";
+    //Reemplaza equipo por el nombre de tu equipo de prácticas
+    public static final int qos = 1;
+    public static final String broker = "tcp://mqtt.eclipse.org:1883";
+    public static final String clientId = "IotTestInfinity123";
+//Reemplaza ClientId con un valor diferente
+}
