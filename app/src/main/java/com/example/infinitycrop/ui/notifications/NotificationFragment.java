bd4fe7f0f380d4ef43afdb0a9179c54a28f17e00
Andrey <andreykuzmin342@gmail.com>
2020-11-18 18:13:54 +0100

    HECHO -> LOGIN+APP (SIN MAPS)

--- app/src/main/java/com/example/infinitycrop/ui/notifications/NotificationFragment.java
+++ app/src/main/java/com/example/infinitycrop/ui/notifications/NotificationFragment.java
@@ -61,6 +61,8 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View v= inflater.inflate(R.layout.fragment_notification, container, false);
+
+        return v;
     }
 }
