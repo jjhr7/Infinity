8606264e8b690a68906aace946ab80bbdeb39071
Quiqueferre12 <quiqueferre12@gmail.com>
2020-11-19 02:13:06 +0100

    INTENTO DE COMUNICACIÓN ENTRE SENSORES , MQTT CON FIRESTORE

--- comun/src/main/java/com/example/comun/Mqtt.java
+++ comun/src/main/java/com/example/comun/Mqtt.java
@@ -2,7 +2,9 @@ package com.example.comun;
 
 public class Mqtt {
     public static final String TAG = "MQTT";
-    public static final String topicRoot="infinity/prueba/";
+    public static final String topicRoot="infinity/senyal/";
+
+
     //Reemplaza equipo por el nombre de tu equipo de prácticas
     public static final int qos = 1;
     public static final String broker = "tcp://mqtt.eclipse.org:1883";
--- things/src/main/java/com/example/things/MainActivity.java
+++ things/src/main/java/com/example/things/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.things;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.comun.Mqtt;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -16,10 +20,13 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.example.comun.Mqtt.qos;
 import static com.example.comun.Mqtt.topicRoot;
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -40,12 +47,79 @@ import static com.example.comun.Mqtt.topicRoot;
  */
 public class MainActivity extends AppCompatActivity implements MqttCallback {
     public static MqttClient client = null;
-
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+        try {
+            client = new MqttClient(Mqtt.broker, Mqtt.clientId, new
+                    MemoryPersistence());
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(true);
+        connOpts.setKeepAliveInterval(60);
+        connOpts.setWill(topicRoot+"WillTopic", "App desconectada".getBytes(),Mqtt.qos, false);
+
+        try {
+            client.connect(connOpts);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+        //conexión con el broker Root1 = Lector de datos
+        //nos subscribimos a topic lectura
+        try {
+            Log.i(Mqtt.TAG, "Subscrito a " + topicRoot + "lectura");//aqui está el root al que nos subscribimos si se quiere modificar se tiene que modificar este
+            client.subscribe(topicRoot + "lectura", Mqtt.qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //Root2 = Canal de senyal de luces
+
+        try {
+            Log.i(Mqtt.TAG, "Subscrito a " + topicRoot + "luces");//aqui está el root al que nos subscribimos si se quiere modificar se tiene que modificar este
+            client.subscribe(topicRoot + "luces", Mqtt.qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+        //--------------------------------------------------------------------------------------------------------------------------------------------
+        //Root 3= Canal de senyal de  ventiladores
+
+        try {
+            Log.i(Mqtt.TAG, "Subscrito a " + topicRoot + "ventiladores");//aqui está el root al que nos subscribimos si se quiere modificar se tiene que modificar este
+            client.subscribe(topicRoot + "ventiladores", Mqtt.qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+        //--------------------------------------------------------------------------------------------------------------------------------------
+        //Root 4= Canal de senyal riego
+
+        try {
+            Log.i(Mqtt.TAG, "Subscrito a " + topicRoot + "riego");//aqui está el root al que nos subscribimos si se quiere modificar se tiene que modificar este
+            client.subscribe(topicRoot + "riego", Mqtt.qos);
+            client.setCallback(this);
+        } catch (MqttException e) {
+            Log.e(Mqtt.TAG, "Error al suscribir.", e);
+        }
+
+
+
+
+        //instancia con el Firestore
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //esto de bajo una vez acabemos con las comunicaciones de los mensajes se podrá borrar
+
         //Datos del sensor de humedad
         Map<String, Object> datosS1 = new HashMap<>();
         datosS1.put("Porcentaje", "%");
@@ -57,8 +131,9 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         datosS2.put("Estado", "Desactivado");
         /* datosS2.put("fecha", System.currentTimeMillis());*/
 
-        //Datos del sensor de la ventilación
+        //Datos del sensor de temperatura en ambiente
         Map<String, Object> datosS3 = new HashMap<>();
+        datosS3.put("Medición", "ºC");
         datosS3.put("Estado", "Desactivado");
 
         //Datos del sensor de temperatura
@@ -67,41 +142,74 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         datosS4.put("Estado:", "Activado");
 
 
-        db.collection("SensoresA-T").document("Humedad").set(datosS1);//colocamos la coleccion y luego el document para enviarse
-        db.collection("SensoresA-T").document("Iluminación").set(datosS2);//colocamos la coleccion y luego el document para enviarse
-        db.collection("SensoresA-T").document("Ventilación").set(datosS3);//colocamos la coleccion y luego el document para enviarse
-        db.collection("SensoresA-T").document("Temperatura").set(datosS4);//colocamos la coleccion y luego el document para enviarse
-        try {
-            Log.i(Mqtt.TAG, "Conectando al broker " + Mqtt.broker);
-            client = new MqttClient(Mqtt.broker, Mqtt.clientId,
-                    new MemoryPersistence());
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            connOpts.setKeepAliveInterval(60);
-            connOpts.setWill(topicRoot + "Desconexión", "App desconectada".getBytes(), Mqtt.qos, false);
-            client.connect(connOpts);
-        } catch (MqttException e) {
-            Log.e(Mqtt.TAG, "Error al conectar.", e);
-        }
-        try {
-            Log.i(Mqtt.TAG, "Publicando mensaje: " + "hola");
-            MqttMessage message = new MqttMessage("Sensores sincronizandose".getBytes());
-            message.setQos(Mqtt.qos);
-            message.setRetained(false);
-            client.publish(topicRoot + "InfinityCrop", message);
-        } catch (MqttException e) {
-            Log.e(Mqtt.TAG, "Error al publicar.", e);
-        }
-        try {
-            Log.i(Mqtt.TAG, "Suscrito a " + topicRoot + "infinity/prueba/");
-            client.subscribe(topicRoot + "POWER", Mqtt.qos);
-            client.setCallback(this);
-        } catch (MqttException e) {
-            Log.e(Mqtt.TAG, "Error al suscribir.", e);
-        }
-
+        db.collection("SensoresA-T").document("Humedad").set(datosS1);//colocamos la coleccion y luego el document para enviarse con los datos
+        db.collection("SensoresA-T").document("Iluminación").set(datosS2);//colocamos la coleccion y luego el document para enviarse con los datos
+        db.collection("SensoresA-T").document("Temperatura ambiente").set(datosS3);//colocamos la coleccion y luego el document para enviarse con los datos
+        db.collection("SensoresA-T").document("Temperatura").set(datosS4);//colocamos la coleccion y luego el document para enviarse con los datos
 
     }
+    // Se ejecuta cuando se pierde la conexión
+    @Override
+    public void connectionLost(Throwable cause) {
+        Log.d(Mqtt.TAG, "Conexión perdida");
+
+    }
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        String payload = new String(message.getPayload());
+        Log.d(Mqtt.TAG, "Recibiendo: " + topic + "->" + payload);
+
+        if(topic.equals(topicRoot+"lectura")){
+            topicLectura(payload);
+        }
+
+        if(topic.equals(topicRoot+"luces")){
+            topicLuces(payload);
+        }
+        if(topic.equals(topicRoot+"riego")){
+            topicRiego(payload);
+        }
+        if(topic.equals(topicRoot+"ventiladores")){
+            topicVentiladores(payload);
+        }
+
+    }
+    private void topicLectura(final String payload) {
+        String[] parts = payload.split("-");//comprueba si los datos introducidos van referentes a cada sensor: nombre,medición, estado
+        db.collection("SensoresA-T").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){// si se han introducido bien los datos exigidos
+                   if(parts[0].equals("Humedad")){
+                       db.collection("SensoresA-T")
+                               .document("Humedad")
+                               .update("Nombre", parts[0] ,"Estado",parts[1],"Porcentaje", parts[2]);//verificar los estados y actualizarlos
+                   }
+                   else {
+                       Log.w(Mqtt.TAG, "Error al obtener documentos.", task.getException());
+                   }
+
+                }
+
+            }
+
+        });
+    }
+
+    private void topicLuces(final String payload) {
+    }
+
+    private void topicRiego(final String payload) {
+    }
+
+    private void topicVentiladores(final String payload) {
+    }
+
+
+
+
+
+
 
     @Override
     public void onDestroy() {
@@ -114,17 +222,9 @@ public class MainActivity extends AppCompatActivity implements MqttCallback {
         super.onDestroy();
     }
 
-    @Override
-    public void connectionLost(Throwable cause) {
-        Log.d(Mqtt.TAG, "Conexión perdida");
 
-    }
 
-    @Override
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
-        String payload = new String(message.getPayload());
-        Log.d(Mqtt.TAG, "Recibiendo: " + topic + "->" + payload);
-    }
+
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken token) {
