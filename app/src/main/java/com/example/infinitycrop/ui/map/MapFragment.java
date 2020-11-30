c1ac3f2b68bea3839a256a21530caf04319c36d0
belen <bgralop@epsg.upv.es>
2020-11-30 15:23:24 +0100

    Eliminado meterse a ajustes en la localizacion

--- app/src/main/java/com/example/infinitycrop/ui/map/MapFragment.java
+++ app/src/main/java/com/example/infinitycrop/ui/map/MapFragment.java
@@ -237,7 +237,21 @@ public class MapFragment extends Fragment implements
                             // fixed by showing the user
                             // a dialog.
                             try {
-                               
+                                    final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                    builder.setMessage("Parece que tienes el GPS desactivado, ¿quieres activarlo?")
+                                            .setCancelable(false)
+                                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                                public void onClick(final DialogInterface dialog, final int id) {
+                                                    EnableGPSAutoMatically();
+                                                }
+                                            })
+                                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                                public void onClick(final DialogInterface dialog, final int id) {
+                                                    dialog.cancel();
+                                                }
+                                            });
+                                    final AlertDialog alert = builder.create();
+                                    alert.show();
                                 status.startResolutionForResult(getActivity(), 1000);
 
                             } catch (IntentSender.SendIntentException e) {
