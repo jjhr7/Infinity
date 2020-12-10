4345b63caf816086c247e6f6174d508e4fe0ce1a
Andrey <andreykuzmin342@gmail.com>
2020-12-10 23:58:27 +0100

    APP v3.1.2 -> FUNCIONA TODO

--- comun/src/main/java/com/example/comun/Mqtt.java
+++ comun/src/main/java/com/example/comun/Mqtt.java
@@ -7,7 +7,7 @@ public class Mqtt {
 
     //Reemplaza equipo por el nombre de tu equipo de prácticas
     public static final int qos = 1;
-    public static final String broker = "tcp://broker.hivemq.com:18883";
+    public static final String broker = "tcp://broker.hivemq.com:1883";
     public static final String clientId = "IotTestInfinity123434";
 //Reemplaza ClientId con un valor diferente
 }
